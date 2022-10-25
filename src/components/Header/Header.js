56fcf3cf8138442ec1e3bff7701bfd8499d362ed
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2022-10-25 11:13:48 +0100

    add tailwind package

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return;
+  <div className="chat-notification">
+    <div className="chat-notification-content">
+      <h4 className="chat-notification-title">ChitChat</h4>
+      <p className="chat-notification-message">You have a new message!</p>
+    </div>
+  </div>;
+};
+
+export default {
+  Header
+};
